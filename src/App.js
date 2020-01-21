1ee7c4547fb4b1d3d1c82fc55fbff7e992eef30f
Frans Jesky <fransjesky@hotmail.com>
2020-01-22 03:10:56 +0700

    0.1.5 - Added Jumbotron

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Logo from "./components/brandLogo";
 import Navbar from "./components/navbar";
 import Carousel from "./components/carousel";
+import Jumbotron from "./components/jumbotron";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       </div>
       <Navbar/>
       <Carousel/>
+      <Jumbotron/>
     </div>
   );
 }
---
+++ src/components/jumbotron.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function jumbotron() {
+  return (
+    <div>
+      <div className="jumbotron jumbotronImage">
+        <h1 className="display-4">Tutup Panci Bistro</h1>
+        <p className="lead textHighlights">
+          Indonesian Fusion Casual Dining in BSD. Serving your dining
+          experience in different way. Tutup Panci can be said to be one of the
+          most unique café located in BSD. This café’s theme takes pan in every
+          arrangement and interior, starting from lamp, wall decoration and
+          display window. Tutup Panci also serves a variety of food, starting
+          from local food to western
+        </p>
+        <hr className="my-4" />
+        <p className="textHighlights">
+          Open everyday: 9.00 AM - 11.00 PM
+          <br />
+          Contact number: 0857 18544637
+          <br />
+          Address: Boulevard Residence, Blok AH Jl. Anggrek Loka, BSD, Serpong,
+          Tangerang Selatan
+        </p>
+        <a className="btn btn-light btn-lg" href="#" role="button">
+          Reservation
+        </a>
+      </div>
+    </div>
+  );
+}
